9806b6a104353e2342a95642ba26f50272f3157e
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-04-22 16:21:03 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("")
+	const [ todos, setTodos ] = useState([])
+	const handleForm = (e) => {
+		e.preventDefault()
+		setTodos(todos.concat(inputValue));
+		
+			setInputValue("");
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			<h1>My Todos {inputValue}</h1>
+			
+				<form onSubmit={handleForm}>
+			<input type="text"
+			onChange={(e) => {setInputValue(e.target.value)}}
+			value={inputValue}
+			placeholder="What to you need to do?"></input>
+			<button style={{opacity:0}} type="submit"></button>
+			</form><ul>
+			{todos?.map((item, index) => (
+				<li key={index}>
+					{item}{""}
+			<FaTrash
+			onClick={() =>
+			setTodos(
+				todos.filter(
+					(t,currentIndex) =>
+					index != currentIndex
+				))}/>
+			</li>
+			))}
+			</ul>
+		
+			<div>{todos?.length}taks</div>
+		</div >
 	);
 };
 
